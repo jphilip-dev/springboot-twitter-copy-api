b18fc5a66a14939c9eea9523f5200c49adc810ae
jphilip-dev <john.p.s.de.roxas+git@gmail.com>
2025-02-05 22:27:04 +0800

    Initial commit

---
+++ src/test/java/com/jphilips/twittercopy/TwitterCopyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jphilips.twittercopy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterCopyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
